d61a32732db58e90efd158b032ed475a275cd77a
Shreesh <shreeshtripathi3@gmail.com>
2026-05-30 12:56:23 +0530

    add input

---
+++ 04_input.py
@@ -0,0 +1,6 @@
+name =  input("Enter your name: ")
+print(f"Hello, {name}!")
+age = input("Enter your age: ")
+age = int(age)  # Convert the age from string to integer
+print(f"You are {age} years old.")
+print(f"Next year, you will be {age + 1} years old.")
